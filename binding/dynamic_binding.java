649efd89585e0133a0f6ce8839a7e6c8a75f4117
Jeevitha <gvselvam0202@gmail.com>
2024-02-13 21:39:55 +0530

    Add files via upload

---
+++ binding/dynamic_binding.java
@@ -0,0 +1,25 @@
+class a{
+	
+public void m1()
+{
+	System.out.println("hii");
+}
+public static void main(String []args)
+	{
+		a i=new b();
+		i.m1();
+		
+	}
+
+
+}
+class b extends a{
+	
+	public void m1()
+	{
+		System.out.println("hello");
+	}
+	
+	
+	
+}
